c94fbe2a923196cb46ee15aefb8dd5a194c6b5d8
Jannek <janneks@tutanota.com>
2023-06-09 17:41:35 +0200

    feat: add date to post preview and sort newest to oldest

--- src/components/PostPreviewSection.jsx
+++ src/components/PostPreviewSection.jsx
@@ -1,6 +1,7 @@
 import PreviewCard from "./PreviewCard";
 
 function PostPreviewSection({ blogPosts }) {
+  const sortedPosts = [...blogPosts].sort((a, b) => a.startDate < b.startDate);
   return (
     <section className="w-full mb-4 lg:w-1/2 min-h-screen py-2 px-4 border-b-2 lg:border-r-2 lg:border-b-0 border-opacity-40 border-orange-800">
       <h2 className="sticky top-14 py-3 text-4xl font-bold font-title text-center text-orange-950 bg-white z-10">
@@ -8,7 +9,7 @@ function PostPreviewSection({ blogPosts }) {
       </h2>
       {blogPosts ? (
         <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 mb-4">
-          {blogPosts.map((post) => (
+          {sortedPosts.map((post) => (
             <PreviewCard blogPost={post} key={post.id} />
           ))}
         </ul>
--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,6 +1,9 @@
 import { Link } from "wouter";
 
 function PreviewCard({ blogPost }) {
+  const start = new Date(blogPost.startDate);
+  const end = new Date(blogPost.endDate);
+
   return (
     <div className="flex flex-col max-w-xl rounded-md shadow-lg bg-amber-50 text-orange-950">
       <div className="relative w-full h-64 rounded-t-md">
@@ -11,25 +14,6 @@ function PreviewCard({ blogPost }) {
         />
         <div className="absolute bottom-0 right-0 left-0 flex flex-row items-center justify-between space-x-2 bg-gradient-to-b from-transparent to-gray-900 p-4 text-orange-50">
           <div className="flex flex-row items-center space-x-1">
-            {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="{1.5}"
-            stroke="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-            />
-          </svg> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -67,7 +51,10 @@ function PreviewCard({ blogPost }) {
               d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
             />
           </svg>
-          <p>July 2018</p>
+          <p>
+            {start.toLocaleDateString("en-GB")} to{" "}
+            {end.toLocaleDateString("en-GB")}
+          </p>
         </div>
         <p className="line-clamp-3 my-4">{blogPost.text}</p>
         <div className="flex flex-row items-center justify-between">
