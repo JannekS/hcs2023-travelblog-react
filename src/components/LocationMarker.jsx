1678612f80cca99e1f79dee8b0be1995249267e6
Jannek <janneks@tutanota.com>
2023-06-10 19:57:40 +0200

    feat: marker popup displays info and link of corresponding blog post

--- src/components/LocationMarker.jsx
+++ src/components/LocationMarker.jsx
@@ -1,5 +1,8 @@
 import { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
+import TravelDuration from "./TravelDuration";
+import AuthorAvatar from "./AuthorAvatar";
+import { Link } from "wouter";
 
 function LocationMarker({ location }) {
   const [showPopup, setShowPopup] = useState(false);
@@ -23,10 +26,32 @@ function LocationMarker({ location }) {
           anchor="bottom"
           offset={30}
           closeOnClick={false}
-          className="w-32 p-1 rounded-md"
+          className="rounded-md"
           onClose={() => setShowPopup(false)}
         >
-          You are here
+          <div className="flex flex-col space-y-2">
+            <h2 className="font-title font-bold text-2xl line-clamp-1">
+              {location.title}
+            </h2>
+            <TravelDuration
+              startDateStr={location.startDate}
+              endDateStr={location.endDate}
+            />
+            <div className="flex flex-row justify-between items-center">
+              <AuthorAvatar
+                id={location.authorId}
+                image={location.authorImg}
+                name={location.authorName}
+                size="sm"
+              />
+              <Link
+                href={`/post/${location.id}`}
+                className="underline underline-offset-4 decoration-2 decoration-orange-800/60 hover:decoration-4 hover:underline-offset-2 focus:none hover:decoration-orange-800/80"
+              >
+                Read more
+              </Link>
+            </div>
+          </div>
         </Popup>
       )}
     </>
