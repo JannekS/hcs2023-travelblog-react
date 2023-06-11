352eba3be5be19556a5b8640d5737b286f7c2453
Jannek <janneks@tutanota.com>
2023-06-11 12:59:12 +0200

    feat: add logo to header bar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,14 @@ import { Link } from "wouter";
 function AppHeader() {
   return (
     <header className="sticky top-0 z-50 flex flex-row justify-between items-center w-full h-14 px-4 py-2 bg-amber-300">
-      <Link href="/">
+      <Link href="/" className="flex flex-row items-center space-x-2">
+        <div className="h-10 w-10">
+          <img
+            src="/footprints.png"
+            alt=""
+            className="h-10 w-12 object-contain"
+          />
+        </div>
         <h1 className="text-3xl font-bold font-handwriting cursor-pointer">
           in the sand
         </h1>
