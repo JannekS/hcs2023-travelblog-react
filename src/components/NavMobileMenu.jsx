594243eed7132463a7778d98a0eaf337c3ffdf05
Jannek <janneks@tutanota.com>
2023-06-14 09:14:39 +0200

    feat: hide mobile menu when login dialog is shown

--- src/components/NavMobileMenu.jsx
+++ src/components/NavMobileMenu.jsx
@@ -1,20 +1,24 @@
 import NavBar from "./NavBar";
 import NavMobileButton from "./NavMobileButton";
 import { useState } from "react";
+import useStore from "../stores/store";
 
-function NavMobileMenu({ onLoginClick }) {
+function NavMobileMenu() {
   const [showMenu, setShowMenu] = useState(false);
+  const showLoginDialog = useStore((state) => state.showLoginDialog);
 
   return (
     <>
       <NavMobileButton toggleMenu={() => setShowMenu(!showMenu)} />
-      <div
-        className={`${
-          !showMenu && "hidden"
-        } absolute top-14 right-0 py-4 px-8 bg-amber-300/90 rounded-b-md`}
-      >
-        <NavBar onLoginClick={onLoginClick} />
-      </div>
+      {!showLoginDialog && (
+        <div
+          className={`${
+            !showMenu && "hidden"
+          } absolute top-14 right-0 py-4 px-8 bg-amber-300/90 rounded-b-md`}
+        >
+          <NavBar />
+        </div>
+      )}
     </>
   );
 }
