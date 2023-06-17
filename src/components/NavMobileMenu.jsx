e10b749d4d756ccd4f7d6f8165df4e13603ccc31
Jannek <janneks@tutanota.com>
2023-06-17 15:47:55 +0200

    fix: correct missing function call for toggling mobile nav menu

--- src/components/NavMobileMenu.jsx
+++ src/components/NavMobileMenu.jsx
@@ -3,21 +3,19 @@ import NavMobileButton from "./NavMobileButton";
 import useStore from "../stores/store";
 
 function NavMobileMenu() {
-  const [showMenu, toggleMenu] = useStore((state) => [
+  const [showMobileMenu, toggleMobileMenu] = useStore((state) => [
     state.showMobileMenu,
     state.toggleMobileMenu,
   ]);
 
   return (
     <>
-      <NavMobileButton toggleMenu={() => toggleMenu} />
-      <div
-        className={`${
-          !showMenu && "hidden"
-        } absolute top-14 right-0 py-4 px-8 bg-amber-200/90 backdrop-blur-sm rounded-b-md`}
-      >
-        <NavBar />
-      </div>
+      <NavMobileButton toggleMenu={() => toggleMobileMenu()} />
+      {showMobileMenu && (
+        <div className="absolute top-14 right-0 py-4 px-8 bg-amber-200/90 backdrop-blur-sm rounded-b-md">
+          <NavBar />
+        </div>
+      )}
     </>
   );
 }
