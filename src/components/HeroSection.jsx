b541c8b873d3e996f3f7cfc56ac4adebe4ca5964
Jannek <janneks@mailbox.org>
2025-03-02 19:15:05 +0100

    fix: ui hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 function HeroSection() {
   return (
-    <section className="flex flex-col justify-center items-center space-y-2 -mt-14 p-4 text-amber-50 text-4xl text-center  w-full h-screen max-h-[400px] md:max-h-screen lg:max-h-[800px] bg-[url('/footprints-sand.jpg')] bg-cover bg-center md:text-6xl md:space-y-3 2xl:h-[50vh] font-handwriting">
+    <section className="flex flex-col justify-center items-center space-y-2 -mt-14 p-4 text-amber-50 text-4xl text-center  w-full h-screen max-h-[400px] md:max-h-screen lg:h-[75vh] lg:max-h-[700px] bg-[url('/footprints-sand.jpg')] bg-cover bg-center md:text-6xl md:space-y-3 2xl:h-[50vh] font-handwriting">
       <h2 className="font-bold">Footprints in the sand</h2>
       <p>are but temporary traces of our journey</p>
       <p>in direct contact to the ground.</p>
